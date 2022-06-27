e064e54f7a111147cc14506761fe0c4f4a1732ef
Rafael Serinolli <rafael.serinolli2@gmail.com>
2022-06-27 10:18:57 -0300

    Deleted old sql file, added new md file

--- DataManinupationLanguage.sql
+++
@@ -1,5 +0,0 @@
-/*Data Manipulation Language
-Te permite modificar as informações presentes na base de dados, seja deletando, modificando ou inserindo*/
-
-INSERT -> Insere informações conforme o lugar especificado
-    INSERT INTO TableName 
